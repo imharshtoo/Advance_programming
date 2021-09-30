305b763823e4694267c664967ac54ebd661a8c8a
harsh <harsh20434@iiitd.ac.in>
2021-09-30 20:17:42 +0530

    initial commiti

---
+++ java_exercise_3.java
@@ -0,0 +1,17 @@
+/* import java.util.Scanner;
+public class java_exercise_3 {
+    public static void main(String[] args) {
+        Scanner input=new Scanner(System.in);
+        //int a=input.nextInt();
+        //thisSystem.out.println(a>8);
+        String b=input.nextLine();
+        System.out.println(b);
+
+
+
+
+        
+    }
+    
+}
+ */
